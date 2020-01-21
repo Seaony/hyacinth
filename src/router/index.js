61760b3ca9b62acd78e141734b0543eb77f22bbe
seaony <seaony@outlook.com>
2020-01-21 17:51:12 +0800

    add vuex and nprogress

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import routes from './routes'
 import Router from 'vue-router'
+import NProgress from 'nprogress'
 
 Vue.use(Router)
 
@@ -18,5 +19,39 @@ function createRouter () {
     mode: 'history'
   })
 
+  router.afterEach(afterEach)
+  router.beforeEach(beforeEach)
+
   return router
+}
+
+/**
+ * Global router guard.
+ *
+ * @param to
+ * @param from
+ * @param next
+ * @param redirect
+ * @returns {Promise<void>}
+ */
+async function beforeEach (to, from, next, redirect) {
+  NProgress.start()
+
+  if (!localStorage.token && to.name.substr(0, to.name.lastIndexOf('.')) !== 'auth') {
+    next({ name: 'auth.login' })
+  } else {
+    next()
+  }
+}
+
+/**
+ * Global after hook.
+ *
+ * @param to
+ * @param from
+ * @param next
+ * @returns {Promise<void>}
+ */
+async function afterEach (to, from, next) {
+  NProgress.done()
 }
